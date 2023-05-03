fd09ce5d7e584d51699b1b530aa7f76c368583cc
ZiaurRehmanmalhi <ziamalhi1234@gmail.com>
2023-05-03 22:24:25 +0500

    django_app

--- products/models.py
+++ products/models.py
@@ -4,19 +4,15 @@ from base.models import BaseModel
 
 class Category(BaseModel):
     category_name = models.CharField(max_length=100)
-    category_name = models.ImageField(upload="categories")
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    category_image = models.ImageField(upload="categories")
 
 
 class Products(BaseModel):
     category_name = models.CharField(nax_length=100)
-    category = models.ForeignKey(Category, on_delete=models.CASED, related_name="products")
-    price = models.ImageField()
-    product_description = models.TextField()
-
-
-class Product(BaseModel):
-    product_name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    price = models.ImageField()
     product_description = models.TextField()
 
 
